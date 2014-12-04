fe181b0dfa87ec6f583a485a13442ff11dd42fb4
Marie Douriez <marie.douriez@gmail.com>
2014-12-04 09:57:12 +0100

    Marie 04/12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DomotiX::Application.config.secret_key_base = 'f00140c43598c0373d30e1ea0fe40d7df1aecdbf29c5243e13a4e57e7acdf6010e29bdbb297ae66283a5f921915f930ce32bda57932d7177c196e35c097d9713'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DomotiX::Application.config.session_store :cookie_store, key: '_DomotiX_session'
